45764c21161c3964d75a5c41db95b5d2c82d3773
Vulturif <vulturif1@gmail.com>
2021-12-26 17:23:59 +0100

    version 0.9

---
+++ ReaderUI/package.bat
@@ -0,0 +1,13 @@
+@echo off
+"C:\Program Files\Java\jdk-17.0.1\bin\jpackage.exe" ^
+--type exe ^
+--verbose ^
+--dest target/package ^
+--name CardReader ^
+--module net.exoa.readerui/net.exoa.readerui.HelloApplication ^
+--runtime-image target/CardReaderImage ^
+--win-dir-chooser ^
+--win-menu ^
+--win-shortcut ^
+--app-version 0.9
+pause
